c1a6f92ce0e721090f6af29b39716f57eece1ee6
Andrey Eriskovskiy 20PI1 <80524924+AndreyEriskovskiy@users.noreply.github.com>
2023-03-13 18:40:58 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,9 @@
 from socket import *
-from random import *
-from threading import *
 import os.path
 import sys
 
 
-class Client(Thread):
+class Client:
     def is_valid_filename(self, filename):
         
         if not os.path.exists(filename):
@@ -67,10 +65,17 @@ else:
 
                         elif reaction_on_the_reg == 'Failed':
                             print('Вы уже были зарегистрированы, пройдите авторизацию')
+                    else:
+                        login, password = '?', '?'
+                        data = login + "_" + password
+                        client_socket.send(data.encode('utf-8'))
+                        reaction_on_the_empty_data = client_socket.recv(1024).decode()
+                        print(reaction_on_the_empty_data)
 
                 elif command == '/auth':
                     counter = 0
                     SuccessFlag = False
+                    EmptyFlag = False
 
                     for i in range(3):
                         login = input('Логин: ')
@@ -85,13 +90,25 @@ else:
                                 print('Вы успешно авторизованы')
                                 SuccessFlag = True
                                 break
+                        
+
 
                             elif reaction_on_the_auth == 'Failed':
                                 if counter != 2:
                                     print('Ошибка авторизации: убедитесь в правильности введённых данных, либо вашей регистрации')
 
                                 counter += 1
-                                    
+                        else:
+                            login, password = '?', '?'
+                            data = login + "_" + password
+                            client_socket.send(data.encode('utf-8'))
+                            EmptyFlag = True
+                            reaction_on_the_empty_data = client_socket.recv(1024).decode()
+                            print(reaction_on_the_empty_data)
+                            break
+
+                    if EmptyFlag:
+                        continue
 
                     if SuccessFlag:
                         break
@@ -122,7 +139,11 @@ else:
                     break
 
                 elif choice == start:
-                    file_name = input('Введите полный путь к текстовому файлу, который вы хотите отправить на сервер: ')              
+                    if version == 1:
+                        file_name = input('Введите полный путь к текстовому файлу, который вы хотите отправить на сервер: ')
+                    else:
+                        file_name = input('Введите полный путь к бинарному файлу, который вы хотите отправить на сервер: ')
+
                     obj = Client()
 
                     if obj.is_valid_filename(file_name):
@@ -134,8 +155,27 @@ else:
                         with open(file_name, 'rb') as f:    
                             send_data = f.read(file_size)
                             client_socket.send(send_data)
-               
+
+                        catalog_msg = client_socket.recv(1024).decode()
+
+                        if catalog_msg != 'NoAdmin':
+                            while True:
+                                user_catalog = input(catalog_msg)
+                                
+                                if user_catalog != '':
+                                    client_socket.send(user_catalog.encode('utf-8'))
+                                    existing_catalog_flag = client_socket.recv(1024).decode()
+                                    
+                                    if existing_catalog_flag == 'NotExist':
+                                        print('Введенного вами каталога не существует, попробуйте еще раз')
+                                        continue
+                                    else:
+                                        break
+                                else:
+                                    print('Вы не ввели имя каталога пользователя')
+
                         msg = client_socket.recv(1024).decode()
+                        
                         if msg != 'True':
                             print(msg)
                         else:
@@ -144,8 +184,6 @@ else:
         
                     else:
                         client_socket.send('Invalid'.encode('utf-8'))
-                        
-            
 
                 else:
                     msg = 'Вы ввели несуществующую команду, попробуйте ещё раз'
