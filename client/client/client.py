362cf1161b1a2a5f4698abd8b9e4bc5d5f42991e
Andrey Eriskovskiy 20PI1 <80524924+AndreyEriskovskiy@users.noreply.github.com>
2023-03-22 19:27:51 +0300

    Update client.py

--- client/client/client.py
+++ client/client/client.py
@@ -1,73 +1,96 @@
+'''
+Необходимые модули для работы с сокетами, файловой системой и системными функциями
+'''
+
 from socket import *
 import os.path
 import sys
 
-
+'''
+Класс Client для работы с клиентскими операциями
+'''
 class Client:
+
+
+    # Метод is_valid_filename() проверяет валидность введенного пользователем пути к файлу
     def is_valid_filename(self, filename):
+        '''
+
+        '''
 
         if os.path.exists(filename) and filename.find('\\' * 2) == -1:
             if os.path.isfile(filename):
                 return True
             else:
                 print('Введенный вами путь не является файлом')
-                return False
+
         else:
             print('Заданного вами пути к файлу не существует')
-            return False
+
+        return False
 
 
 host = '127.0.0.1'
 port = 1111
 
+# Создание клиентского сокета
 try:
     client_socket = socket(AF_INET, SOCK_STREAM)
 except error:
     print('Ошибка создания клиентского сокета')
 
+# Подключение к серверу
 try:
     client_socket.connect((host, port))
 except error:
     print('Ошибка подключения к серверу')
 
 else:
-    flag_in_server = client_socket.recv(1024).decode()
 
-    if flag_in_server == 'Auth':
+    # Реакция сервера на подключение клиента
+    flag_in_to_the_server = client_socket.recv(1024).decode()
+
+    if flag_in_to_the_server == 'Auth':  # Успешное подключение
         print('На начальном этапе вам необходимо авторизоваться, либо зарегистрироваться')
         while True:
             print('Для регистрации используется команда /reg, для авторизации - /auth')
             command = input('Введите команду: ')
-            client_socket.send(command.encode('utf-8'))
+            client_socket.send(command.encode('utf-8'))  # отправка команды, введенной пользователем, на сервер
 
+            # выбрана регистрация
             if command == '/reg':
                 login = input('Логин: ')
                 password = input('Пароль: ')
 
+                # данные были введены пользователем
                 if login != '' and password != '':
                     data = login + "_" + password
-                    client_socket.send(data.encode('utf-8'))
-                    reaction_on_the_reg = client_socket.recv(1024).decode()
+                    client_socket.send(data.encode('utf-8'))  # отправка логина и пароля на сервер
+                    reaction_on_the_reg = client_socket.recv(1024).decode() # реакция сервера на введенные данные пользователя
 
+                    # Успешная регистрация
                     if reaction_on_the_reg == 'Successful':
                         print('Вы успешно зарегистрированы')
                         break
 
+                    # Ошибка регистрации
                     elif reaction_on_the_reg == 'Failed':
                         print('Вы уже были зарегистрированы, пройдите авторизацию')
                 else:
+                    # данные не были введены пользователем
                     login, password = '?', '?'
                     data = login + "_" + password
                     client_socket.send(data.encode('utf-8'))
                     reaction_on_the_empty_data = client_socket.recv(1024).decode()
                     print(reaction_on_the_empty_data)
 
+            # выбрана авторизация
             elif command == '/auth':
-                counter = 0
-                SuccessFlag = False
-                EmptyFlag = False
+                counter = 0               # счетчик попыток авторизации пользователя
+                SuccessFlag = False       # флаг успешной авторизации
+                EmptyFlag = False         # флаг о невведенных пользователем логине или пароле
 
-                for i in range(3):
+                for i in range(3):        # 3 попытки на авторизацию пользователя, в противном случае закрывается соединение с сервером
                     login = input('Логин: ')
                     password = input('Пароль: ')
 
@@ -80,8 +103,6 @@ else:
                             print('Вы успешно авторизованы')
                             SuccessFlag = True
                             break
-
-
 
                         elif reaction_on_the_auth == 'Failed':
                             if counter != 2:
@@ -104,21 +125,21 @@ else:
                     break
 
                 if counter == 3:
-                    print('Вы потратили все попытки для прохождения авторизации на сервере :(')
+                    print('Вы потратили все попытки для прохождения авторизации на сервере')
                     sys.exit(0)
-
 
             else:
                 print('Введена несуществующая команда, повторите еще раз')
     else:
-        print(flag_in_server)
+        # Превышено количество подключений к серверу
+        print(flag_in_to_the_server)
         sys.exit(0)
+
     print('''Данная программа обеспечивает:
 1) передачу текстовых файлов на сервер, если версия программы-сервера равна 1
 2) передачу бинарных файлов на сервер, если версия программы-сервера равна 2''')
-    version = client_socket.recv(1024).decode()
+    version = int(client_socket.recv(1024).decode())  # версия сервера
     print('Серверная программа работает в версии:', version)
-    version = int(version)
     print('Поддерживаемые режимы работы программы: /start, /exit')
     start = '/start'
     exit = '/exit'
@@ -126,7 +147,7 @@ else:
     if version:
         while True:
             choice = input('Введите режим работы: ')
-            client_socket.send(choice.encode('utf-8'))
+            client_socket.send(choice.encode('utf-8'))  # отправка команды (/start, /exit) на сервер
 
             if choice == exit:
                 print('Программа успешно завершена')
@@ -140,34 +161,36 @@ else:
 
                 obj = Client()
 
+                # введенный путь к файлу действителен
                 if obj.is_valid_filename(file_name):
-                    client_socket.send('Valid'.encode('utf-8'))
-                    client_socket.send(file_name.encode('utf-8'))
+                    client_socket.send('Valid'.encode('utf-8'))  # отправка сообщения о валидности передаваемого файла
+                    client_socket.send(file_name.encode('utf-8'))  # отправка пути к файлу
                     pos = file_name.rfind('\\')
                     file_size = os.path.getsize(file_name)
 
                     with open(file_name, 'rb') as f:
                         send_data = f.read(file_size)
-                        client_socket.send(send_data)
+                        client_socket.send(send_data)    # отправка содержимого файла на сервер
 
-                    msg = client_socket.recv(1024).decode()
+                    msg = client_socket.recv(1024).decode()  # сообщение о состоянии файла, переданного на сервер
 
+                    # Файл не соответствует версии программы-сервера
                     if msg != 'True':
                         print(msg)
 
                     else:
 
-                        catalog_msg = client_socket.recv(1024).decode()
+                        status = client_socket.recv(1024).decode()  # сообщение о статусе пользователя
 
-                        if catalog_msg != 'NoAdmin':
+                        if status == 'Admin':
                             while True:
                                 users = client_socket.recv(1024).decode()
                                 print(f'Возможные каталоги для сохранения: {users}')
-                                user_catalog = input(catalog_msg)
+                                user_catalog = input('Введите имя каталога пользователя, в который хотите сохранить файл: ')
 
                                 if user_catalog != '':
-                                    client_socket.send(user_catalog.encode('utf-8'))
-                                    existing_catalog_flag = client_socket.recv(1024).decode()
+                                    client_socket.send(user_catalog.encode('utf-8'))  # отправка выбранного каталога на сервер
+                                    existing_catalog_flag = client_socket.recv(1024).decode()  # сообщение о наличии (отсутствии) пользовательского каталога на сервере
 
                                     if existing_catalog_flag == 'NotExist':
                                         print('Введенного вами каталога не существует, попробуйте еще раз')
@@ -183,7 +206,7 @@ else:
 
 
                 else:
-                    client_socket.send('Invalid'.encode('utf-8'))
+                    client_socket.send('Invalid'.encode('utf-8')) # отправка сообщения о невалидности передаваемого файла
 
             else:
                 msg = 'Вы ввели несуществующую команду, попробуйте ещё раз'
